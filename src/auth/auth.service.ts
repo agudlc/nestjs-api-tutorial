25e8c40495ea495219a3a57ddac00e28e670309e
agudlc <87042363+agudlc@users.noreply.github.com>
2022-06-02 19:30:44 -0300

    auth login and structure advanced

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { User, Bookmark } from "@prisma/client"
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+
+@Injectable()
+export class AuthService{
+
+    constructor(private prisma: PrismaService) {}
+    
+    async signup (dto: AuthDto) {
+
+        try {
+        const hash = await argon.hash(dto.password)
+
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+        })
+
+        delete user.hash;
+
+        return user;
+        } catch (error) {
+            if ( error instanceof PrismaClientKnownRequestError) {
+                if(error.code === "P2002") {
+                    throw new ForbiddenException("CredentialsTaken")
+                }
+            }
+            throw error;
+            }
+        }
+
+    async signin (dto: AuthDto) {
+
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        })
+
+
+        return "i am signin in"
+    }
+
+}
